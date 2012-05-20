9d1d90c70d041dbcabcc334c68394a769f69607e
Samir Talwar <samir@noodlesandwich.com>
2012-05-20 14:10:21 +0100

    Implemented the background.

--- sideways.js
+++ sideways.js
@@ -1,3 +1,19 @@
-(function() {
+setTimeout(function() {
+    function setup(callback) {
+        setTimeout(callback, 0);
+    }
 
-})();
+    setup(function() {
+        backdrop('skyblue');
+    });
+
+    var scene = sjs.Scene({
+            w: document.body.offsetWidth,
+            h: document.body.offsetHeight
+        }),
+        background = scene.Layer('background', {color: 'white'});
+
+    function backdrop(color) {
+        background.setColor(color);
+    }
+}, 0);
