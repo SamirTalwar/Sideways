ce1f00da698269c9e13edfc05dd0084442f4b63e
Samir Talwar <samir@noodlesandwich.com>
2012-05-20 16:29:48 +0100

    Ground.

--- sideways.js
+++ sideways.js
@@ -1,19 +1,40 @@
 setTimeout(function() {
+    var width = document.body.offsetWidth,
+        height = document.body.offsetHeight,
+
+        blockWidth = 101,
+        blockHeight = 171,
+        blockDepth = 42;
+
     function setup(callback) {
         setTimeout(callback, 0);
     }
 
     setup(function() {
         backdrop('skyblue');
+        ground();
     });
 
-    var scene = sjs.Scene({
-            w: document.body.offsetWidth,
-            h: document.body.offsetHeight
-        }),
+    var scene = sjs.Scene({w: width, h: height}),
         background = scene.Layer('background', {color: 'white'});
+        foreground = scene.Layer('foreground');
 
     function backdrop(color) {
         background.setColor(color);
+    }
+
+    function ground() {
+        foreground.Sprite("PlanetCute/DirtBlock.png", {
+            x: 0,
+            y: height - blockHeight,
+            w: width,
+            h: blockHeight
+        }).update();
+        foreground.Sprite("PlanetCute/GrassBlock.png", {
+            x: 0,
+            y: height - (blockHeight + blockDepth),
+            w: width,
+            h: blockHeight
+        }).update();
     }
 }, 0);
